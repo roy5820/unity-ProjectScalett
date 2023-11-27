7593bcc50a2ed6fbb3c89c1d166adac6189f4649
roy5820 <35182721+roy5820@users.noreply.github.com>
2023-11-28 03:43:16 +0900

    시계 퀴즈 수정

--- Assets/Script/Puzzle/ClockPuzzle/ClockHandController.cs
+++ Assets/Script/Puzzle/ClockPuzzle/ClockHandController.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
 
-public class ClockHandController : MonoBehaviour, IDragHandler
+public class ClockHandController : MonoBehaviour, IDragHandler, IEndDragHandler
 {
     public void OnDrag(PointerEventData eventData)
     {
@@ -14,5 +15,11 @@ public class ClockHandController : MonoBehaviour, IDragHandler
         Vector3 direction = worldPos - transform.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log(transform.GetComponent<RectTransform>().rotation.z * 180 / 6);
+        transform.rotation = Quaternion.Euler(0, 0, (int)(transform.GetComponent<RectTransform>().rotation.z*180/6) * 30);
     }
 }
